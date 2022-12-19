e7d0233f592e5ecea05bb7139279e5c8e6db24ac
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-12-19 12:15:56 -0500

    map stuff

--- src/components/LocalBusiness.js
+++ src/components/LocalBusiness.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import {useJsApiLoader, GoogleMap} from "@react-google-maps/api"
+import React, {useState} from 'react'
+import {useJsApiLoader, GoogleMap, Marker} from "@react-google-maps/api"
 
 function LocalBusiness() {
+
+const [map, setMap] = useState(null)
 
 const {isLoaded} = useJsApiLoader({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
@@ -13,13 +15,41 @@ if(!isLoaded) {
 
 // console.log(isLoaded)
 
-const center = {lat: 48.8584, lng: 2.2945}
+const center = {lat: 40.7986, lng: -74.2391}
 
     return (
-        <div className='google-maps'>
-            <GoogleMap center={center} zoom={15} mapContainerStyle={{width: '100%', height: '100%'}}>
+        <div className='map-header'>
+            <h1>Search Local Businesses</h1>
+            <form className='map-form' >
+            <label>Email:</label>
+                <input 
+                type="text" 
+                name="Email"
+                
+                />
 
+                <label>Password:</label>
+                <input 
+                type="text" 
+                name="password"
+                
+                />
+                <br></br>
+                <br></br>
+                <input type="submit" value="Login"/>
+            </form>
+
+        <div className='google-maps'>
+            <GoogleMap center={center} 
+            zoom={15} 
+            mapContainerStyle={{width: '100%', height: '100%'}}
+            onLoad={(map) => setMap(map)}
+            >
+                <Marker position={center}/>
+                
             </GoogleMap>
+        </div>
+
         </div>
     )
 }
